7e591af4376b1f6b4f130a30e74ee95db829f260
DanielJDufour <daniel.j.dufour@gmail.com>
2021-12-28 12:48:36 -0500

    added test case

--- test.js
+++ test.js
@@ -57,3 +57,16 @@ test("xSkew = null", ({ eq }) => {
   }
   eq(error.message, "[wld-writer] xSkew is null");
 });
+
+test("write super precise numbers", ({ eq }) => {
+  const data = {
+    xScale: "2445.9849051249894000077777777777777",
+    ySkew: 0,
+    xSkew: 0,
+    yScale: -2445.98490512499,
+    xOrigin: 7699959.850241235,
+    yOrigin: 1323859.6754601842
+  };
+  const written = write(data);
+  eq(written, "2445.9849051249894000077777777777777\n0\n0\n-2445.98490512499\n7699959.850241235\n1323859.6754601842\n");
+});
